ad3e24d613bea8bc8f68122aee61d03fc4945396
Hannah Horobin <horobinhl@gmail.com>
2020-02-17 12:51:05 -0500

    event form only shows when signed in

--- app/serializers/ballot_show_serializer.rb
+++ app/serializers/ballot_show_serializer.rb
@@ -1,7 +1,15 @@
 class BallotShowSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :state_id
+  attributes :id, :name, :description, :state_id, :signed_in
 
   has_many :events
   belongs_to :state
+
+  def signed_in
+    if scope
+      scope
+    else
+      false
+    end
+  end
 
 end

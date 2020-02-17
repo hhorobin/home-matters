967fbbd4c2976fa3f85f6b7661a8a5f7a03aba38
Hannah Horobin <horobinhl@gmail.com>
2020-02-17 11:47:11 -0500

    handling non-signed in users responses

--- app/controllers/api/v1/responses_controller.rb
+++ app/controllers/api/v1/responses_controller.rb
@@ -3,14 +3,18 @@ class Api::V1::ResponsesController < ApplicationController
   def create
     event = Event.find(params["event_id"])
     responses = Response.where(event: params[:event_id])
-    existing = responses.where(user: current_user)
+    if user_signed_in?
+      existing = responses.where(user: current_user)
 
-    if existing == []
-      Response.create(
-        event: event,
-        user: current_user
-      )
-      render json: responses
+      if existing == []
+        Response.create(
+          event: event,
+          user: current_user
+        )
+        render json: responses
+      else
+        render json: responses
+      end
     else
       render json: responses
     end
--- app/serializers/responses_serializer.rb
+++ app/serializers/responses_serializer.rb
@@ -1,6 +1,10 @@
 class ResponsesSerializer < ActiveModel::Serializer
-  attributes :id, :event_id, :user_id
+  attributes :id, :event_id, :user_id, :current_user
 
   belongs_to :event
   belongs_to :user
+
+  def current_user
+    current_user?
+  end
 end

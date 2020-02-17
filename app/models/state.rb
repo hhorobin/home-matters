dec18aa8ebb25b0f31b1581df7716cebe34fef89
Hannah Horobin <horobinhl@gmail.com>
2020-02-17 14:18:09 -0500

    map with state abbreviations

--- app/models/state.rb
+++ app/models/state.rb
@@ -2,6 +2,4 @@ class State < ApplicationRecord
   validates :name, presence: true
 
   has_many :ballots
-  #make a constant ABBREVIATIONS = write them all out. it would be a hash with key value abbrev, full name
-
 end
